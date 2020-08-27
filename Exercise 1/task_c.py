134a192452ee04f4f125e3d865d46c23fe51a036
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-08-27 14:55:19 +0200

    Finished assignment. Added taskC_figure.PNG. Removed '# ' from header in day_head_circumference.csv and changed 'head circumference' to 'headCircumference'

--- Exercise 1/task_c.py
+++ Exercise 1/task_c.py
@@ -1,43 +1,43 @@
 import torch
 import matplotlib.pyplot as plt
-import _csv
 import pandas as pd
 
-learning_rate = 0.0001
-epoch_amount = 65000
+learning_rate = 0.00000001
+epoch_amount = 50000
 
 
-class LinearRegressionModel:
+class NonLinearRegressionModel:
     def __init__(self):
         # Model variables
-        self.W = torch.tensor([[0.0], [0.0]], requires_grad=True)  # requires_grad enables calculation of gradients
+        # requires_grad enables calculation of gradients
+        self.W = torch.tensor([[0.0]], requires_grad=True)
         self.b = torch.tensor([[0.0]], requires_grad=True)
 
     # Predictor
     def f(self, x):
-        return x @ self.W + self.b  # @ corresponds to matrix multiplication
+        return 20 * torch.sigmoid(x @ self.W + self.b) + 31
 
     # Uses Mean Squared Error
     def loss(self, x, y):
-        return torch.nn.functional.mse_loss(self.f(x), y)  # Can also use torch.nn.functional.mse_loss(self.f(x), y) to possibly increase numberical stability
+        return torch.nn.functional.mse_loss(self.f(x), y)
 
 
-data = pd.read_csv("csv/day_length_weight.csv")
+data = pd.read_csv("csv/day_head_circumference.csv")
 
-x_data = [data["length"].tolist(), data["weight"].tolist()]
-y_data = data["day"].tolist()
+x_data = data["day"].tolist()
+y_data = data["headCircumference"].tolist()
 
 
 # Observed/training input and output
-x_train = torch.tensor(x_data, dtype=torch.float).t()
+x_train = torch.tensor(x_data, dtype=torch.float).reshape(-1, 1)
 
-y_train = torch.tensor(y_data, dtype=torch.float).t().reshape(-1, 1)
+y_train = torch.tensor(y_data, dtype=torch.float).reshape(-1, 1)
 
 
-model = LinearRegressionModel()
+model = NonLinearRegressionModel()
 
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
-optimizer = torch.optim.SGD([model.W, model.b], learning_rate)
+optimizer = torch.optim.SGD([model.b, model.W], learning_rate)
 
 for epoch in range(epoch_amount):
     model.loss(x_train, y_train).backward()  # Compute loss gradients
@@ -52,17 +52,16 @@ for epoch in range(epoch_amount):
 print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
 
 # Visualize result
-figure = plt.figure().gca(projection='3d')
-
 # Plot data points
-figure.scatter(data["length"].tolist(), data["weight"].tolist(), data["day"].tolist(), c='red')
+plt.plot(x_train, y_train, 'o', label='$(\\hat x^{(i)},\\hat y^{(i)})$')
 
-# Plot Regression line
-figure.scatter(data["length"].tolist(), data["weight"].tolist(), model.f(x_train).detach(), label='$y = f(x) = xW+b$')
+# set x-label
+plt.xlabel('Age (days)')
+# set y-label
+plt.ylabel('Head circumference (unknown)')
 
-# Plot labels
-figure.set_xlabel('Length (cm)')
-figure.set_ylabel('Weight (kg)')
-figure.set_zlabel('Age (days)')
+x, indices = torch.sort(x_train, 0)
+plt.plot(x, model.f(x).detach(), label='$y = f(x) = 20*σ(xW+b)+31$', c="red")
 
+plt.legend()
 plt.show()
